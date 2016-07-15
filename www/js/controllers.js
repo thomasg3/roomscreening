89f4cbd81f0f80f7b5e93c74ba4979df24d5b2a6
thomas_ge <thomasgeboers@gmail.com>
2016-07-15 13:12:28 +0200

    Clean up controllers to a specific file. Small bugfix

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,114 +1,11 @@
-angular.module('roomscreening.controllers', [])
+angular.module('roomscreening.controllers', ['roomscreening.controllers.screenings'])
 
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
 })
 
 
-.controller('ScreeningOverviewController', function($rootScope, $scope, $stateParams ,LocalScreeningService, $ionicPopup){
 
-  var array = function(screenings){
-    return Object.keys(screenings).map(function(id){return screenings[id];});
-  }
-
-  $scope.screenings = LocalScreeningService.getAll();
-  $scope.screeningsArray = array($scope.screenings);
-
-  $scope.$on('$ionicView.enter', function(e){
-    $scope.screenings = LocalScreeningService.getAll();
-    $scope.screeningsArray = array($scope.screenings);
-    $scope.$apply();
-  })
-
-  $scope.select = function(id){
-    $scope.selectedId = id;
-    $rootScope.$broadcast('screeningSelected', $scope.screenings[id]);
-  };
-
-  $scope.isSelected = function(id){
-    return $scope.selectedId == id;
-  };
-
-  $scope.$on('screeningDetailReady', function(){
-    if($stateParams.id != null){
-      $scope.select($stateParams.id);
-    } else {
-      $scope.select(screeningsArray[0].id);
-    }
-  });
-
-  $scope.swipeRight = function(){
-    $scope.showDelete = true;
-  }
-
-  $scope.swipeLeft = function(){
-    $scope.showDelete = false;
-  }
-
-  $scope.remove = function(id){
-    var confirmPopup = $ionicPopup.confirm({
-      title: "Screening Verwijderen",
-      template: "Bent u zeker dat u deze screening wilt verwijderen?",
-      cancelText: 'Annuleer'
-    }).then(function(confirmed){
-      if(confirmed){
-        LocalScreeningService.remove(id);
-        $scope.screenings = LocalScreeningService.getAll();
-        $scope.screeningsArray = array($scope.screenings);
-        if($scope.selectedId == id){
-          $scope.select();
-        }
-        $scope.showDelete = false;
-      } else {
-        $scope.showDelete = false;
-      }
-    })
-
-  }
-
-
-
-})
-
-.controller('ScreeningDetailController', function($rootScope, $scope){
-  $scope.$on('screeningSelected', function(event, screening){
-    $scope.screening = screening;
-  });
-  $rootScope.$broadcast('screeningDetailReady');
-})
-
-.controller('ScreeningEditController', function($scope, LocalScreeningService, $state, $stateParams,$ionicHistory){
-
-  $scope.clients = [
-    {name: "Zorro"},
-    {name: "Curt"},
-    {name: "Alice"},
-    {name: "Bob"},
-    {name: "Denise"},
-    {name: "Engelbert"},
-    {name: "Fons"}
-  ];
-
-  if($stateParams.id != null){
-    $scope.screening = LocalScreeningService.get($stateParams.id);
-  } else {
-    $scope.screening = {};
-  }
-  $scope.save = function(){
-    $scope.screening.last_edit = new Date();
-    if($stateParams.id != null){
-      LocalScreeningService.update($scope.screening);
-      var id = $stateParams.id;
-    } else {
-      $scope.screening.rooms = [];
-      var id = LocalScreeningService.add($scope.screening);
-    }
-    $ionicHistory.nextViewOptions({
-      historyRoot: true
-    });
-    $state.go('app.specific_screening', {id: id});
-  };
-})
 
 
 ;
---
+++ www/js/controllers/screening.js
@@ -0,0 +1,104 @@
+angular.module('roomscreening.controllers.screenings', [])
+
+.controller('ScreeningOverviewController', function($rootScope, $scope, $stateParams ,LocalScreeningService, $ionicPopup){
+
+  var array = function(screenings){
+    return Object.keys(screenings).map(function(id){return screenings[id];});
+  }
+
+  var reset = function(){
+    $scope.screenings = LocalScreeningService.getAll();
+    $scope.screeningsArray = array($scope.screenings);
+  }
+
+  $scope.select = function(id){
+    $scope.selectedId = id;
+    $rootScope.$broadcast('screeningSelected', $scope.screenings[id]);
+  };
+
+  $scope.isSelected = function(id){
+    return $scope.selectedId == id;
+  };
+
+  $scope.$on('screeningDetailReady', function(){
+    if($stateParams.id != null){
+      $scope.select($stateParams.id);
+    } else {
+      $scope.select($scope.screeningsArray[0].id);
+    }
+  });
+
+  $scope.swipeRight = function(){
+    $scope.showDelete = true;
+  }
+
+  $scope.swipeLeft = function(){
+    $scope.showDelete = false;
+  }
+
+  $scope.remove = function(id){
+    var confirmPopup = $ionicPopup.confirm({
+      title: "Screening Verwijderen",
+      template: "Bent u zeker dat u deze screening wilt verwijderen?",
+      cancelText: 'Annuleer'
+    }).then(function(confirmed){
+      if(confirmed){
+        LocalScreeningService.remove(id);
+        $scope.screenings = LocalScreeningService.getAll();
+        $scope.screeningsArray = array($scope.screenings);
+        if($scope.selectedId == id){
+          $scope.select();
+        }
+        $scope.showDelete = false;
+      } else {
+        $scope.showDelete = false;
+      }
+    })
+
+  }
+
+  reset();
+})
+
+.controller('ScreeningDetailController', function($rootScope, $scope){
+  $scope.$on('screeningSelected', function(event, screening){
+    $scope.screening = screening;
+  });
+
+  $rootScope.$broadcast('screeningDetailReady');
+})
+
+.controller('ScreeningEditController', function($scope, LocalScreeningService, $state, $stateParams,$ionicHistory){
+  $scope.clients = [
+    {name: "Zorro"},
+    {name: "Curt"},
+    {name: "Alice"},
+    {name: "Bob"},
+    {name: "Denise"},
+    {name: "Engelbert"},
+    {name: "Fons"}
+  ];
+
+  if($stateParams.id != null){
+    $scope.screening = LocalScreeningService.get($stateParams.id);
+  } else {
+    $scope.screening = {};
+  }
+
+  $scope.save = function(){
+    $scope.screening.last_edit = new Date();
+    if($stateParams.id != null){
+      LocalScreeningService.update($scope.screening);
+      var id = $stateParams.id;
+    } else {
+      $scope.screening.rooms = [];
+      var id = LocalScreeningService.add($scope.screening);
+    }
+    $ionicHistory.nextViewOptions({
+      historyRoot: true
+    });
+    $state.go('app.specific_screening', {id: id});
+  };
+})
+
+;
