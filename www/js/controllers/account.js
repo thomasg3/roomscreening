548cb5b1a7ec70dbcb74c2643d7c9096fe0952a4
thomas_ge <thomasgeboers@gmail.com>
2016-07-15 13:47:51 +0200

    Add Login Modal

--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -1,6 +1,12 @@
 angular.module('roomscreening.controllers.account', [])
-  .controller('LoginController', function($scope){
-    $scope.helloworld = "Hello";
+  .controller('LoginController', function($scope, $ionicModal){
+
+    $ionicModal.fromTemplateUrl('templates/account/login.modal.html', {
+      scope: $scope
+    }).then(function(modal){
+      modal.show();
+    })
+
   })
 
 ;
