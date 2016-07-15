84b6457af4d86cd478e510c81762c83b5380655b
thomas_ge <thomasgeboers@gmail.com>
2016-07-15 14:27:24 +0200

    Add Login view

--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -1,13 +1,22 @@
 angular.module('roomscreening.controllers.account', [])
   .controller('LoginController', function($scope, $ionicModal){
 
-    $ionicModal.fromTemplateUrl('templates/account/login.modal.html', {
-      scope: $scope,
-      backdropClickToClose: false,
-      hardwareBackButtonClose: false
-    }).then(function(modal){
-      modal.show();
-    })
+
+    if(!$scope.loginModal){
+      $ionicModal.fromTemplateUrl('templates/account/login.modal.html', {
+        scope: $scope,
+        backdropClickToClose: false,
+        hardwareBackButtonClose: false
+      }).then(function(modal){
+        $scope.loginModal = modal;
+        $scope.loginModal.show();
+        $scope.loading = false;
+      })
+    }
+
+    $scope.doLogin = function(){
+      $scope.loading = true;
+    }
 
 
 
