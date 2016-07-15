497907c88d2214e8aaa6a2691ba4aeca2a3daacb
thomas_ge <thomasgeboers@gmail.com>
2016-07-15 14:07:52 +0200

    Solve bug for vanishing Edit Button in Screening details

--- www/js/controllers/account.js
+++ www/js/controllers/account.js
@@ -2,10 +2,14 @@ angular.module('roomscreening.controllers.account', [])
   .controller('LoginController', function($scope, $ionicModal){
 
     $ionicModal.fromTemplateUrl('templates/account/login.modal.html', {
-      scope: $scope
+      scope: $scope,
+      backdropClickToClose: false,
+      hardwareBackButtonClose: false
     }).then(function(modal){
       modal.show();
     })
+
+
 
   })
 
