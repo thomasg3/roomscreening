6cc7fc6e8772befee429117ee74dc05b0a85a24a
thomas_ge <thomasgeboers@gmail.com>
2016-07-20 09:50:06 +0200

    Add StructureService based on ngResource

--- www/js/services.js
+++ www/js/services.js
@@ -86,5 +86,8 @@ angular.module('roomscreening.services', [])
       }
     }
   })
+  .factory('StructureService', function(baseURL, $resource){
+    return $resource(baseURL+'structure');
+  })
 
 ;
